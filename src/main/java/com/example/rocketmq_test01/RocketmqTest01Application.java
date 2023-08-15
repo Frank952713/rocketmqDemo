9ae3d0e7039e6437cbda436747543d7a97fe463e
wangzhiwei <wangzhiwei4@grgbanking.com>
2023-08-15 16:46:35 +0800

    生产者异步发送

---
+++ src/main/java/com/example/rocketmq_test01/RocketmqTest01Application.java
@@ -0,0 +1,13 @@
+package com.example.rocketmq_test01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RocketmqTest01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RocketmqTest01Application.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/rocketmq_test01/reciver/MyPullConsumer.java
@@ -0,0 +1,32 @@
+package com.example.rocketmq_test01.reciver;
+
+import org.apache.rocketmq.client.consumer.DefaultMQPullConsumer;
+import org.apache.rocketmq.client.consumer.PullResult;
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.common.message.MessageExt;
+import org.apache.rocketmq.common.message.MessageQueue;
+
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.Set;
+
+
+public class MyPullConsumer {
+    public static void main(String[] args) throws Exception {
+        DefaultMQPullConsumer consumer = new DefaultMQPullConsumer("consumer_0");
+        consumer.setNamesrvAddr("127.0.0.1:9876");
+        consumer.start();
+
+        Set<MessageQueue> messageQueueSet = consumer.fetchSubscribeMessageQueues("tp_2");
+        for (MessageQueue m : messageQueueSet) {
+            PullResult result = consumer.pull(m, "*", 0, 10);
+            System.out.println("消息消息：" + m);
+
+            List<MessageExt> msgFoundList = result.getMsgFoundList();
+            for (MessageExt ext : msgFoundList) {
+                System.out.println(new String(ext.getBody(), StandardCharsets.UTF_8));
+            }
+        }
+        consumer.shutdown();
+    }
+}
